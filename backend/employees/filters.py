a23a99d77dccec2cfe4b89167356d431e247bb71
lalit8694 <lalit8694123@gmail.com>
2025-07-30 12:33:38 +0530

    Update file for filters

--- backend/employees/filters.py
+++ backend/employees/filters.py
@@ -5,9 +5,17 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
         firstname = django_filters.CharFilter(field_name='firstname', lookup_expr='iexact')
         email = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
+        # #between range we can filter
+        # int_min=django_filters.CharFilter(method='filter_by_id_range')
+        # int_max=django_filters.CharFilter(method='filter_by_id_range')
 
         
         class Meta:
             model = Employee
             fields = ['firstname','email']
+        
+        # def filter_by_id_range(self, queryset, name, value,):
+              
+            
+            
 
